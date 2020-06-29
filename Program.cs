cec097febece367227b61e64eea9095688db82f6
ana-111 <anacarolinaamaralsilva2003@gmail.com>
2020-06-29 09:55:51 -0300

    projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_27_28_29_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
